230e5fe2bad27a17b2f24acd25c3ae880cce3249
Christian Davel Altan Garrido <CDAltan@Desarrollo.administracion.local>
2018-09-19 10:47:04 -0600

    c4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { SumaService } from '../servicios/suma.service';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  
+  procesos = [
+    {id: 1, name: 'proceso-1', },
+    {id: 2, name: 'proceso-2' },
+    {id: 3, name: 'proceso-3' },
+    {id: 4, name: 'proceso-4' }  
+  ];
 
-  constructor() { }
+  procesoActual: String;
+  procesoEstado: String;
+  procesoInicio: number;
+  procesoResultado: number;
+ 
+
+  constructor(private sumaService: SumaService) { }
 
   ngOnInit() {
+    this.procesoActual= "Suma"
+    this.procesoEstado= "Activo";  
+    this.getValueWithAsync2();      
+  } 
+
+  async getValueWithAsync2() {
+    this.procesoInicio = 7;
+    const value = <number>await this.sumaService.Adding(this.procesoInicio);
+    console.log(`async result: ${value}`);
+    this.procesoResultado = value;
   }
-
-
 }
--- src/app/servicios/suma.service.ts
+++ src/app/servicios/suma.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { delay } from 'q';
 
 @Injectable()
 export class SumaService {
 
+ 
   constructor() { }
 
+  Adding(total: number){    
+    let total2: number;
+    return new Promise(resolve => {          
+        for (let i=1; i < 10; i++ ){
+          setTimeout(() => {
+              total =7+total;     
+              console.log(total);                  
+          },i*1000);           
+        }
+        setTimeout(() => {
+          resolve(total);
+        }, 10000);
+                
+    });
+  }
+
+ 
 }
+
+
